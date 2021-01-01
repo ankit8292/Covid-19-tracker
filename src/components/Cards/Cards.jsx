9132154100a1a99f494e9df1af28f76863d713f5
Ankit <ankit@ankit.com>
2021-01-01 20:23:55 +0530

    files uploaded

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -9,8 +9,7 @@ const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
   }
 
   return (
-    <div className={styles.container}>
-        <Typography gutterBottom variant="h4" component="h2"></Typography>
+    <div className={styles.container}>  
       <Grid container spacing={3} justify="center">
         <CardComponent
           className={styles.infected}
